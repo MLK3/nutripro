7867dd2dd6376b45723dbefe9e5c1b467b075d4f
Marcelo Li Koga <marcelo.koga@polilibras.com.br>
2016-12-28 10:16:48 -0200

    getDiet()

--- src/test/java/br/com/nutriproapp/model/DietTest.java
+++ src/test/java/br/com/nutriproapp/model/DietTest.java
@@ -16,16 +16,15 @@ public class DietTest {
     @Test
     public void testEnergyRequeriments() {
         User MAS = new User("Marcelo", 29, Gender.MALE, 173, 73.4, ActivityLevel.SEDENTARY);
-        User FAL = new User("Leticia", 23, Gender.FEMALE, 160, 52.1, ActivityLevel.LOW_ACTIVE);
-        User MCA = new User("Matheus", 15, Gender.MALE, 170, 65.0, ActivityLevel.ACTIVE);
+        User MCL = new User("Matheus", 15, Gender.MALE, 170, 65.0, ActivityLevel.LOW_ACTIVE);
         User FCV = new User("Fernanda", 13, Gender.FEMALE, 151, 47.5, ActivityLevel.VERY_ACTIVE);
+        User FAA = new User("Leticia", 23, Gender.FEMALE, 160, 52.1, ActivityLevel.ACTIVE);
               
         
         assertEquals(2486.9, Diet.getEnergyRequeriment(MAS), 0.1);
-        assertEquals(2174.2, Diet.getEnergyRequeriment(FAL), 0.1);
-        assertEquals(3306.0, Diet.getEnergyRequeriment(MCA), 0.1);
+        assertEquals(2880.8, Diet.getEnergyRequeriment(MCL), 0.1);
         assertEquals(2701.0, Diet.getEnergyRequeriment(FCV), 0.1);
-        
+        assertEquals(2289.6, Diet.getEnergyRequeriment(FAA), 0.1);
     }
     
 }
